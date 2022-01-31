a873486db4ad42be1f34f696a6b1dfe9d84abed5
Aksa@4321 <ghazan.usman@aksa-sds.com>
2022-01-31 18:32:29 +0500

    Project initiated

--- src/app/Modules/account/right-section/services/account.service.ts
+++ src/app/Modules/account/right-section/services/account.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import { Router } from '@angular/router';
+import {environment} from "../../../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-
-  constructor() { }
+  //
+  // constructor(private http: HttpClient , private _router: Router) {
+  // }
+  //
+  // login(user: any) {
+  //   return this.http.post<any>(environment.USER_API_URL + 'user/auth/sign-in', user);
+  // }
+  //
+  //
+  // register(user: any) {
+  //   return this.http.post<any>(environment.USER_API_URL + 'user/auth/sign-up', user);
+  // }
+  //
+  // loggedIn() {
+  //   return !!localStorage.getItem('token');
+  // }
+  // loggedOut() {
+  //   localStorage.removeItem('token');
+  //   return this._router.navigate(['/login']);
+  //
+  // }
+  //
+  // sendOTP(otp_data: any) {
+  //   return this.http.post<any>(environment.USER_API_URL + 'user/auth/send-otp', otp_data);
+  // }
+  //
+  // logOut() {
+  //   localStorage.removeItem('token');
+  // }
+  //
+  // getToken() {
+  //   return localStorage.getItem('token');
+  // }
 }
