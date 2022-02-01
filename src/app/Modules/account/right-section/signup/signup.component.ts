20ce3070d68e85ddf8ff06ab7e1660dead5de00a
Aksa@4321 <ghazan.usman@aksa-sds.com>
2022-02-01 17:42:01 +0500

    Project initiated

--- src/app/Modules/account/right-section/signup/signup.component.ts
+++ src/app/Modules/account/right-section/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {AccountService} from "../services/account.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  signupForm!: FormGroup
+  constructor(
+    private account_service: AccountService,
+    private _formBuilder: FormBuilder,
+    private router: Router) {
+  }
 
   ngOnInit(): void {
+    this.signupForm = this._formBuilder.group({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required])
+    });
   }
 
 }
