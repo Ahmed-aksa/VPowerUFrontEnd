46ad6713ed5cb678b304520590a753078cb57c5e
nabeelhafiz-dev <95689846+nabeelhafiz-dev@users.noreply.github.com>
2022-02-01 17:58:05 +0500

    reactive form

--- src/app/Modules/account/right-section/signup/signup.component.ts
+++ src/app/Modules/account/right-section/signup/signup.component.ts
@@ -19,7 +19,18 @@ export class SignupComponent implements OnInit {
   ngOnInit(): void {
     this.signupForm = this._formBuilder.group({
       name: new FormControl('', [Validators.required]),
-      email: new FormControl('', [Validators.required])
+      email: new FormControl('', [Validators.required]),
+      number: new FormControl('', [Validators.required]),
+      mpan: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      cpassword: new FormControl('', [Validators.required]),
+      address: new FormControl('', [Validators.required]),
+      pdik: new FormControl('', [Validators.required]),
+      mdik: new FormControl('', [Validators.required]),
+      adik: new FormControl('', [Validators.required]),
+      uflex: new FormControl('', [Validators.required]),
+      dflex: new FormControl('', [Validators.required]),
+      ftype: new FormControl('', [Validators.required]),
     });
   }
 
