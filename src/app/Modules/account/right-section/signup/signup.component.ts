07529e6aa1df95a02d77e3f0ee2090ded596d7b1
Aksa@4321 <ghazan.usman@aksa-sds.com>
2022-02-04 12:21:14 +0500

    Project initiated

--- src/app/Modules/account/right-section/signup/signup.component.ts
+++ src/app/Modules/account/right-section/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {AccountService} from "../services/account.service";
 import {Router} from "@angular/router";
@@ -24,33 +24,50 @@ export class SignupComponent implements OnInit {
       name:['', [Validators.required, ]],
       email: new FormControl('', [Validators.required, Validators.email]),
       number: new FormControl('', [Validators.required,]),
-      password: new FormControl('', [Validators.required,]),
-      cpassword: new FormControl('', [Validators.required,]),
-      address: new FormControl('', [Validators.required]),
-      mpan: new FormControl('', ),
-      pdik: new FormControl('', ),
-      mdik: new FormControl('', ),
-      adik: new FormControl('', ),
-      uflex: new FormControl('', ),
-      dflex: new FormControl('', ),
-      ftype: new FormControl('', ),
-    });
+        password: new FormControl('', [Validators.required,]),
+        cpassword: new FormControl('', [Validators.required,]),
+        address: new FormControl('', [Validators.required]),
+        mpan: new FormControl('',),
+        pdik: new FormControl('',),
+        mdik: new FormControl('',),
+        adik: new FormControl('',),
+        uflex: new FormControl('',),
+        dflex: new FormControl('',),
+        ftype: new FormControl('',),
+      }
+      ,
+      {
+        Validators: this.MustMatch('password','cpassword')
+      });
   }
 
   get f() { return this.signupForm.controls; }
 
   onSubmit() {
     this.submited = true;
-    if (this.signupForm.value.password != this.signupForm.value.cpassword){
-      this.passwordMatch = true;
-    }
 
     // stop here if form is invalid
     if (this.signupForm.invalid) {
        return
     }
 
-//     // display form values on success
-//     alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.signupForm.value, null, 4));
-// }
-  }}
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.signupForm.value, null, 4));
+}
+
+ MustMatch(password: string, cPassword: string) {
+    return(formGroup: FormGroup) => {
+      const control = formGroup.controls[password];
+      const matchingControl = formGroup.controls[cPassword];
+      if (matchingControl.errors && !matchingControl.errors.MustMatch){
+        return
+      }
+      if (control.value !== matchingControl.value){
+        matchingControl.setErrors({MustMatch: true})
+      }
+      else{
+        matchingControl.setErrors(null);
+      }
+    }
+  }
+}
